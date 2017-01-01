c8c0620ecb2cac1a42bb29f10a8e4015d28315cc
toshi0607 <s.toshi0607@gmail.com>
2017-01-01 13:00:34 +0900

    add nand gate

--- ch02/weighting_and_bias.py
+++ ch02/weighting_and_bias.py
@@ -9,3 +9,13 @@ def AND(x1, x2):
         return 0
     else:
         return 1
+
+def NAND(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([-0.5, -0.5])
+    b = 0.7
+    tmp = np.sum(w*x) + b
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
